06f77c9fc4a793030984aca7b7cf7c44f8369cea
Your Name <you@example.com>
2025-04-23 21:44:53 +0600

    Add a Loop and a random Function

--- js/script.js
+++ js/script.js
@@ -5,12 +5,26 @@ names.map(items=>{
     console.log(`Welcome ${items}`);    
 })
 
+//Anoter use of Loops
+let animals = ["Dog","Cat","Cow","Horse","Monkey","Elephant"]
+for(let i=1 ; i<animals.length ; i++){
+    console.log(`I love ${animals[1]}`);
+    
+}
 
 //Using Function
 function total(x,y){
     console.log(x+y);
 }
 total(6,7) // The answer will be 13
+
+//Another way to call the function
+function total2(){
+    let a = 8
+    let b = 9
+    console.log(a+b);
+}
+total2() // The answer will be 17
 
 
 //Using another loop with functions
